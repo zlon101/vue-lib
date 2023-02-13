1475204be5b7f7df7d86288873c3ecf8546b3bb2
chenzhilong <chenzhilong@zhuozhuo.io>
2023-02-13 11:51:47 +0800

    提交test

--- src/test/range.vue
+++ src/test/range.vue
@@ -1,25 +1,30 @@
 <template>
   <div class="fwq">
-    <section class="range">
-      <h2>搜索高亮</h2>
-      <input :value="searchText" @change="onSearch" />
-      <p id="p1" ref="p">
-        abcd
-        <b>Hello</b>
-        World!
-      </p>
+    <p>
+      For example,
+      <code>regexp</code>
+      would regex, and using a regex is likely.
+    </p>
+    <p>
+      hello
+      <b>world</b>
+    </p>
 
-      <p id="p2" ref="p2" style="display: none">
-        是的发送到发
-        <b>24离开家 水电费</b>
-        234232
-      </p>
+    <p class="uieq">
+      axasg23
+      <span>4hibb</span>
+      cHec
+    </p>
+    <p class="sdwasd">
+      SVG 能够优雅而简洁地渲染不同大小的图形，并和CSS、DOM、JavaScript 和 SMIL 等其他网络标准无缝衔接。本质上，SVG
+      相对于图像，就好比 HTML 相对于文
+    </p>
 
-      <div class="hide" style="display: none"></div>
-      <div class="hide" style="visibility: hidden"></div>
-      <div class="hide" style="opacity: 0"></div>
-    </section>
-    <section class="j9p">lloWo</section>
+    <!--
+    <div class="hide" style="display: none"><span>display-none</span></div>
+    <div class="hide" style="visibility: hidden"><span>hidden</span></div>
+    <div class="hide" style="opacity: 0"><span>透明度为0</span></div>
+    -->
   </div>
 </template>
 
@@ -28,151 +33,259 @@
 
 const log = console.debug;
 
+export const HighLightElementClass = 'zl_highlight_span';
+export const MatchEleCls = 'zl_search_ele';
+
+const getInnerText = (() => {
+  const p = document.createElement('p');
+  p.style.cssText = 'position:fixed;z-index:-99999;opacity:0;';
+  document.body.appendChild(p);
+  return str => {
+    p.textContent = str;
+    return p.innerText;
+  };
+})();
+
+/**
+ * Object<isAllMatch, isCase>
+ * **/
+const DefaultCfg = {
+  isCase: true,
+  isAllMatch: false,
+  color: 'yellow',
+};
+
+export function traverseDoc(searchText, searchParam = DefaultCfg) {
+  let startTime = Date.now();
+  // 清除上次搜索结果
+  clearLastMark();
+  if (!searchText) return null;
+
+  const [reg] = createRegExp(searchText, searchParam);
+  if (!reg.test(document.body.innerText)) {
+    return null;
+  }
+  console.debug('reg', reg);
+
+  // 遍历所有 Text 节点  🔥
+  const treeWalker = document.createNodeIterator(document.body, NodeFilter.SHOW_TEXT); // createTreeWalker
+  const getStacksText = nodes => getInnerText(nodes.reduce((acc, cur) => acc + cur.wholeText, ''));
+  const isMatch = _txt => {
+    reg.lastIndex = 0;
+    return reg.test(_txt);
+  };
+
+  let curNode = null,
+    stackNodes = [],
+    stackText = '',
+    curNodeText = '';
+  const allRanges = [];
+  reg.lastIndex = 0;
+
+  while (curNode = treeWalker.nextNode()) {
+    curNodeText = curNode.wholeText;
+    if (!/\S/.test(curNodeText) || isHideElement(curNode.parentElement)) {
+      continue;
+    }
+    // 拼接字符串
+    stackNodes.push(curNode);
+    stackText = getStacksText(stackNodes);
+    if (isMatch(stackText)) {
+      let startNode = null;
+      do {
+        startNode = stackNodes.shift();
+      } while ( isMatch( getStacksText(stackNodes) ) );
+      stackNodes.unshift(startNode);
+      // 确定Text节点和偏移
+      const ranges = findOffset(stackNodes, reg);
+      // log('🔥ranges:', ranges);
+      allRanges.push(...ranges);
+      if (stackNodes.length === 1) {
+        stackNodes = [];
+      } else {
+        stackNodes = [stackNodes.pop()];
+      }
+    }
+  }
+
+  const matchHtmls = new Array(allRanges.length);
+  let count = 0;
+  for (const range of allRanges.reverse()) {
+    matchHtmls[count++] = surroundContents(range, searchParam);
+  }
+  console.debug('traverseDoc耗时:', (Date.now() - startTime) / 1000);
+  return matchHtmls.filter(Boolean).reverse();
+}
+
+function findOffset(stackNodes, reg) {
+  const N = stackNodes.length;
+  if (N === 0) {
+    throw new Error('调用findOffset时，参数stackNodes数组长度为0');
+  }
+  // 匹配的文本在一个Text中
+  const startNode =stackNodes[0],
+    endNode = stackNodes[N - 1];
+  let startText = startNode.wholeText,
+    endText = endNode.wholeText,
+    startOffset = 0,
+    endOffset = endText.length;
+  if (N === 1) {
+    const ranges = [];
+    for (const _matchItem of [...startText.matchAll(reg)]) {
+      // debugger;
+      startOffset = _matchItem.index;
+      endOffset = _matchItem.index + _matchItem[0].length - 1;
+      ranges.push({ startNode, endNode, startOffset, endOffset  });
+    }
+    return ranges;
+  }
+
+  // 跨节点
+  const midNodeText = stackNodes.slice(1, -1).reduce((acc, cur) => acc + cur.wholeText, '');
+  const isMatch = _txt => {
+    reg.lastIndex = 0;
+    return reg.test(_txt);
+  };
+
+  // 二分法搜索优化
+  startOffset = dichotomy(startText.length, false, 0, _offset => {
+    return isMatch(getInnerText(startText.slice(_offset)  + midNodeText + endText))
+  });
+  // debugger;
+
+  startText = startText.slice(startOffset);
+  endOffset = dichotomy(endText.length, true, endText.length - 1, _offset => {
+    return isMatch(getInnerText(startText + midNodeText + endText.slice(0, _offset+1)))
+  });
+  // debugger;
+  return [{ startNode, endNode, startOffset, endOffset  }];
+}
+
+function dichotomy(N, toLeft, offsetInd, matchFn) {
+  if (N < 2) {
+    return offsetInd;
+  }
+  let nextInd = toLeft ? offsetInd - 1 : offsetInd + 1;
+  nextInd = Math.max( Math.min(nextInd, N - 1), 0);
+  let lastInd = toLeft ? offsetInd + 1 : offsetInd - 1;
+  lastInd = Math.max( Math.min(lastInd, N - 1), 0);
+
+  const isNextMatch = matchFn(nextInd);
+  // 指针位于起始位置
+  if (offsetInd === lastInd && !isNextMatch) {
+    // debugger;
+    return offsetInd;
+  }
+  // 指针到达终点
+  if (offsetInd === nextInd) {
+    // debugger;
+    return offsetInd;
+  }
+
+  const isCurrentMatch = matchFn(offsetInd);
+  if (isCurrentMatch && !isNextMatch) {
+    // debugger;
+    return offsetInd;
+  }
+
+  let midIndex = 0;
+  if (isCurrentMatch) {
+    midIndex = toLeft ? Math.floor(offsetInd * 0.5) : Math.floor(offsetInd + 0.5 * (N - offsetInd));
+  } else {
+    midIndex = !toLeft ? Math.floor(offsetInd * 0.5) : Math.floor(offsetInd + 0.5 * (N - offsetInd));
+  }
+  return dichotomy(N, toLeft, midIndex, matchFn);
+}
+
+let uid = 1;
+function surroundContents(rangeCfg, searchParam) {
+  let { startNode, startOffset, endNode, endOffset } = rangeCfg;
+  if (startNode && endNode) {
+    // 必须是text类型的节点
+    if ([startNode, endNode].some(_node => _node.nodeType !== 3)) {
+      throw new Error('rangeStart 或 rangeEnd 节点不是 text 类型');
+    }
+    const range = document.createRange();
+    range.setStart(startNode, startOffset);
+    range.setEnd(endNode, endOffset + 1);
+
+    const span = document.createElement('span');
+    span.classList.add(HighLightElementClass);
+    span.style.cssText = `background-color:${searchParam.color};`;
+
+    span.appendChild(range.extractContents());
+    range.insertNode(span);
+    const parentEle = range.commonAncestorContainer;
+
+    const splits = parentEle.innerHTML.split(/<span\s+class="zl_highlight_span".*<\/span>/).map(item => item.trim());
+    const firstLength = splits[0].length;
+    let innerHtml = splits[0].slice(Math.max(0, firstLength-30)) + span.outerHTML + splits[1].slice(0, 30);
+    const cls = `${MatchEleCls}_${uid++}`;
+    parentEle.classList.add(cls);
+    return { innerHtml, cls };
+  }
+  console.debug('开始节点或结束节点为null');
+  return null;
+}
+
+function createRegExp(searchText, param) {
+  if (!searchText) return null;
+  let isRegMode = false
+  let reg = null;
+  if (/^\//.test(searchText)) {
+    log('正则模式');
+    isRegMode = true;
+    const regModifier = /\/(\w*)$/;
+    let modifier = regModifier.exec(searchText)[1];
+    !modifier.includes('g') && (modifier += 'g');
+    regModifier.lastIndex = 0;
+    reg = new RegExp(searchText.slice(1).replace(regModifier, ''), modifier);
+  } else {
+    if (param.isAllMatch) {
+      searchText = `\\b${searchText}\\b`;
+    }
+    reg = new RegExp(searchText, param.isCase ? 'gm' : 'gmi');
+  }
+  return [reg, isRegMode];
+}
+
 function isHideElement(element) {
-  if (!element.offsetHeight || !element.offsetWidth) {
+  if (!element || element.offsetHeight < 2 || element.offsetWidth < 2) {
     return true;
   }
+  let hide = false;
+  if (typeof element.checkVisibility === 'function') {
+    return !element.checkVisibility({
+      checkOpacity: true,      // Check CSS opacity property too
+      checkVisibilityCSS: true // Check CSS visibility property too
+    });
+  }
   const styleAttr = window.getComputedStyle(element);
-  if (styleAttr.display === 'none'
-  || styleAttr.visibility === 'hidden'
-  || styleAttr.opacity === '0') {
-    return true;
+  return styleAttr.display === 'none' || styleAttr.visibility === 'hidden' || styleAttr.opacity === '0';
+}
+
+function isHideNode(node) {
+  // Element
+  if (node.nodeType === 1) {
+    return isHideElement(node);
+  }
+  // Text
+  if (node.nodeType === 3) {
+    return isHideElement(node.parentElement)
   }
   return false;
 }
 
-function traverseDoc(searchVal = 'oWo') {
-  const treeWalker = document.createTreeWalker(document.body, NodeFilter.SHOW_ELEMENT);
-  const reg = new RegExp(searchVal, 'i');
-  if (!reg.test(treeWalker.root.innerText)) {
-    return false;
+function clearLastMark() {
+  const startTime = Date.now();
+  for (const highEle of document.querySelectorAll(`.${HighLightElementClass}`)) {
+    const parent = highEle.parentElement;
+    highEle.outerHTML = highEle.innerText;
+    parent.normalize();
   }
-
-  const currentIsRoot = () => treeWalker.currentNode === treeWalker.root;
-  let curNode = treeWalker.nextNode();
-  reg.lastIndex = 0;
-  let matchEle = null;
-  const result = [];
-  log('开始遍历');
-  console.time();
-  while (curNode && !currentIsRoot()) {
-    const innerText = curNode.innerText;
-    if (isHideElement(curNode) || !reg.test(curNode.innerText)) {
-      matchEle && result.push(matchEle);
-      matchEle = null;
-      curNode = treeWalker.nextSibling();
-      while (!curNode && !currentIsRoot()) {
-        // 没有下一个兄弟节点, 当前 current node 没有变化
-        // treeWalker.previousNode();
-        treeWalker.parentNode();
-        curNode = treeWalker.nextSibling();
-      }
-    } else {
-      matchEle = curNode;
-      matchEle = curNode;
-      curNode = treeWalker.nextNode();
-    }
-    reg.lastIndex = 0;
-  }
-  matchEle && result.push(matchEle);
-
-  const targetEles = [];
-  let N = result.length;
-  result.forEach((node, ind) => {
-    if (ind === N - 1 || !node.contains(result[ind + 1])) {
-      targetEles.push(node);
-    }
-  });
-
-  // log(`
-  // nodeList: %o
-  // targetEles: %o
-  // `, nodeList, targetEles);
-
-  for (const ele of targetEles) {
-    reg.lastIndex = 0;
-    surroundContents(ele, reg.exec(ele.innerText)[0].trim());
-  }
-  console.timeEnd();
+  console.debug('clearLastMark 耗时:', (Date.now()-startTime)/1000);
 }
 
-
-function surroundContents(ele, matchText) {
-  log('surroundContents start');
-  let rangeStart = null,
-    rangeEnd = null,
-    curNode = null,
-    fullText = '',
-    nodeStack = [],
-    regRes = null;
-
-  const NodeIterator = document.createNodeIterator(ele, NodeFilter.SHOW_TEXT);
-
-  while (curNode = NodeIterator.nextNode()) {
-    nodeStack.push(curNode);
-    fullText = nodeStack.map(node => node.wholeText.trim()).join('');
-    if (fullText.includes(matchText)) {
-      rangeEnd = { node: curNode };
-      let startNode;
-      do {
-        startNode = nodeStack.shift();
-        fullText = nodeStack.map(node => node.wholeText.trim()).join('');
-      } while (fullText.includes(matchText));
-      rangeStart = { node: startNode };
-      break;
-    }
-  }
-
-  let startInd = 0,
-    startText = rangeStart.node.wholeText,
-    endText = rangeEnd.node.wholeText,
-    endInd = endText.length - 1;
-
-  // 空格换行处理？
-  if (rangeStart.node && rangeStart.node === rangeEnd.node) {
-    startInd = startText.split(matchText)[0].length;
-    endInd = startInd + matchText.length - 1;
-  } else {
-    while (startInd < startText.length && !matchText.includes(startText.slice(startInd))) {
-      ++startInd;
-    }
-    while (endInd > 0 && !matchText.includes(endText.slice(0, endInd))) {
-      --endInd;
-    }
-  }
-  rangeStart.offset = startInd;
-  rangeEnd.offset = endInd;
-
-  // log(`
-  //   startInd: ${rangeStart.offset}
-  //   ${startText.slice(startInd)}
-  //   endInd: ${rangeEnd.offset}
-  //   ${endText.slice(0, endInd)}
-  //   ${rangeStart.node === rangeEnd.node}
-  // `);
-
-  if (rangeStart && rangeEnd) {
-    // 必须是text类型的节点
-    if ([rangeStart.node, rangeEnd.node].some(_node => _node.nodeType !== 3)) {
-      throw new Error('rangeStart 或 rangeEnd 节点不是 text 类型');
-    }
-    if (rangeStart.node === rangeEnd.node) {
-      rangeEnd.offset++;
-    }
-    const range = document.createRange();
-    range.setStart(rangeStart.node, rangeStart.offset);
-    range.setEnd(rangeEnd.node, rangeEnd.offset);
-
-    const span = document.createElement('span');
-    span.style.cssText = 'background-color:red;font-size:larger';
-
-    span.appendChild(range.extractContents());
-    range.insertNode(span);
-  } else {
-    console.debug('rangeStart 或 rangeEnd 为null');
-  }
-  log('surroundContents end');
-}
 
 export default {
   data() {
@@ -181,21 +294,7 @@ export default {
     };
   },
   mounted() {
-    // traverseDoc('a');
-    return;
-
-    const p1 = this.$el.querySelector('#p1');
-    const startNode = p1.childNodes[1].firstChild;
-    const endNode = startNode;
-
-    const range = document.createRange();
-    range.setStart(startNode, 1);
-    range.setEnd(startNode, 4);
-
-    const span = document.createElement('span');
-    span.style.cssText = 'background-color:red;font-size:larger';
-    span.appendChild(range.extractContents());
-    range.insertNode(span);
+    // traverseDoc('图形');
   },
   methods: {
     onSearch(e) {
@@ -210,6 +309,9 @@ export default {
 </script>
 
 <style>
+body {
+  border: 2px blue solid;
+}
 input {
   border: 1px solid red;
   border-radius: 4px;
@@ -219,5 +321,14 @@ input {
   width: 100px;
   height: 200px;
   outline: 1px solid red;
+}
+.drap {
+  outline: 1px solid red;
+  height: 50px;
+  width: 50px;
+  border-right: 20%;
+  position: absolute;
+  right: 20px;
+  top: 20px;
 }
 </style>
