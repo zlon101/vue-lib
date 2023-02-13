8372054241dae9e0848eb782a3b36bcc82c7eab2
chenzhilong <chenzhilong@zhuozhuo.io>
2023-02-13 14:10:16 +0800

    添加级联组件

--- packages/basecmp/icon/src/icon.js
+++ packages/basecmp/icon/src/icon.js
@@ -268,6 +268,27 @@ export const IconFold = {
   },
 };
 
+const IconFold2DefaultFill = {
+  '0': 'currentColor'
+};
+const IconFold2DefaultStroke = {};
+export const IconFold2 = {
+  mixins: [mixin],
+  computed: {
+    mergeFill() {
+      return { ...IconFold2DefaultFill, ...(this.fill || {}) };
+    },
+    mergeStroke() {
+      return { ...IconFold2DefaultStroke, ...(this.stroke || {}) };
+    },
+  },
+  render(h) {
+    return (<svg style={this.mergeStyle} onClick={this.handleClick}  viewBox="64 64 896 896" width="1em" height="1em" fill={this.mergeFill[0]} xmlns="http://www.w3.org/2000/svg">
+    <path d="M765.7 486.8L314.9 134.7A7.97 7.97 0 00302 141v77.3c0 4.9 2.3 9.6 6.1 12.6l360 281.1-360 281.1c-3.9 3-6.1 7.7-6.1 12.6V883c0 6.7 7.7 10.4 12.9 6.3l450.8-352.1a31.96 31.96 0 000-50.4z"></path>
+</svg>);
+  },
+};
+
 const IconGithubDefaultFill = {
   '0': '#161614',
   '1': '#FFFFFF'
---
+++ packages/business/cascader/example/usage.vue
@@ -0,0 +1,24 @@
+<template>
+  <AreaSelect :root="AreaRoot" />
+</template>
+
+<script>
+import AreaSelect from '../src/index';
+import AreaJson from '../area.json';
+
+export default {
+  components: {
+    AreaSelect,
+  },
+  data() {
+    return {
+      AreaRoot: AreaJson,
+    };
+  },
+  methods: {
+    onChange(range) {
+      console.log(range);
+    },
+  },
+};
+</script>
